3d6b8198cdd1f755ceaad18e183e329c63a1bda1
WBX <290548180@qq.com>
2019-03-27 10:20:09 +0800

    shellSort mergeSort heapSort

--- src/main/java/com/test/sort/SortApplication.java
+++ src/main/java/com/test/sort/SortApplication.java
@@ -1,9 +1,11 @@
 package com.test.sort;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SortApplication {
 
     public static void main(String[] args) {
